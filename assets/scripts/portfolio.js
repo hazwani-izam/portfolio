f095320fb0b6b0aab9aeb822ebb92e2552dc588d
kyrakyun <hazwani.izam@gmail.com>
2020-09-06 14:22:34 +0800

    New Pages and Fixes
    Red Cross

--- assets/scripts/portfolio.js
+++ assets/scripts/portfolio.js
@@ -22,7 +22,7 @@ var _portfolioArr = [
         'tags':'Web Design'
       },
       {
-        'title':'STT eGreeting Cards',
+        'title':'STT Greeting Cards',
         'image':'STT.png',
         'link':'stt',
         'tags':'Graphic Design, Animation'
@@ -62,6 +62,12 @@ var _portfolioArr = [
         'image':'Muay-Thai-Burger@2x.jpg',
         'link':'muay-thai-burger',
         'tags':'Advertising'
+      },
+      {
+        'title':'Red Cross Annual Report',
+        'image':'Red-Cross@2x.jpg',
+        'link':'red-cross',
+        'tags':'Editorial'
       }
     ]
   }
@@ -102,7 +108,7 @@ function populatePortfolio () {
     var _portfolioItems = _portfolioArr[i].portfolioItem;
     for( var j=0; j<_portfolioItems.length; j++) {
       _sidebarStr += '<a href="#card'+_accordionId+'_'+(j+1)+'"><div class="portfolio-trigger accordion-trigger list-group-item" data-target="'+ (i+1) +'" id="portfoliosideItem_'+(i+1)+'">'+_portfolioItems[j].title+'</div></a>';
-      _accordionStr += '<div data-aos="fade-up" data-aos-duration="500" class="card active" id="card'+_accordionId+'_'+(j+1)+'"> <button aria-controls="'+_accordionId+'_'+(j+1)+'" aria-expanded="false" class="btn btn-link" data-target="#'+_accordionId+'_'+(j+1)+'" data-toggle="" type="button"> <div class="card-header" id="'+_accordionId+'-'+(j+1)+'"> <div class="row justify-content-between align-items-center"> <div class="col"> <h2>'+_portfolioItems[j].title+'</h2> <img src="assets/images/header@2x.png"> </div> </div> </div> </button> <div aria-labelledby="'+_accordionId+'-'+(j+1)+'" class=" show " id="'+_accordionId+'_'+(j+1)+'"> <a href="portfolio/'+_portfolioItems[j].link+'.html" target="_blank"> <div class="card-body bg-black"> <img src="assets/images/portfolio/thumbnails/Thumbnail_'+_portfolioItems[j].image+'"> <div class="tags">'+_portfolioItems[j].tags+'</div> </div> </a> </div> </div>';
+      _accordionStr += '<div data-aos="fade-up" data-aos-duration="500" class="card active anchor" id="card'+_accordionId+'_'+(j+1)+'"> <button aria-controls="'+_accordionId+'_'+(j+1)+'" aria-expanded="false" class="btn btn-link" data-target="#'+_accordionId+'_'+(j+1)+'" data-toggle="" type="button"> <div class="card-header" id="'+_accordionId+'-'+(j+1)+'"> <div class="row justify-content-between align-items-center"> <div class="col"> <h2>'+_portfolioItems[j].title+'</h2> <img src="assets/images/header@2x.png"> </div> </div> </div> </button> <div aria-labelledby="'+_accordionId+'-'+(j+1)+'" class=" show " id="'+_accordionId+'_'+(j+1)+'"> <a href="portfolio/'+_portfolioItems[j].link+'.html" target="_blank"> <div class="card-body bg-black"> <img src="assets/images/portfolio/thumbnails/Thumbnail_'+_portfolioItems[j].image+'"> <div class="tags">'+_portfolioItems[j].tags+'</div> </div> </a> </div> </div>';
     }
       _accordionStr +=  '</div>';
   }
