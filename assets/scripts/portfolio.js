266ff73662fac34a1d5cd0103af90960c8b1f26a
Hazwani <hazwani.izam@gmail.com>
2024-05-28 01:00:00 +0800

    updated thumbnails

--- assets/scripts/portfolio.js
+++ assets/scripts/portfolio.js
@@ -17,13 +17,13 @@ var _portfolioArr = [
       },
       // {
       //   'title':'Mercedes-Benz: The Concept EQG',
-      //   'image':'Mercedes-Concept-EQG.jpg',
+      //   'image':'Mercedes-Concept-EQG@2x.jpg',
       //   'link':'mercedes',
       //   'tags':'Events'
       // },
       {
         'title':'Galderma PR Video',
-        'image':'Galderma.jpg',
+        'image':'Galderma@2x.jpg',
         'link':'galderma',
         'tags':'Graphic Design, Animation'
       },
@@ -35,20 +35,26 @@ var _portfolioArr = [
       },
       {
         'title':'WWF',
-        'image':'WWF@2x.png',
+        'image':'WWF@2x.jpg',
         'link':'wwf',
         'tags':'Web Design'
       },
       {
         'title':'UOBAM Invest',
-        'image':'UOBAM-Invest.jpg',
+        'image':'UOBAM-Invest@2x.jpg',
         'link':'uobam-invest',
         'tags':'Web Design'
       },
       {
         'title':'The Scientist',
-        'image':'The-Scientist.jpg',
+        'image':'The-Scientist@2x.jpg',
         'link':'the-scientist',
+        'tags':'Web Design'
+      },
+      {
+        'title':'Natura',
+        'image':'Natura@2x.jpg',
+        'link':'natura',
         'tags':'Web Design'
       },
       {
@@ -61,12 +67,6 @@ var _portfolioArr = [
         'title':'UOB Brand Quiz',
         'image':'UOB-Brand-Quiz@2x.png',
         'link':'uob-brand-quiz',
-        'tags':'Web Design'
-      },
-      {
-        'title':'Natura',
-        'image':'Natura@2x.jpg',
-        'link':'natura',
         'tags':'Web Design'
       },
       {
@@ -151,12 +151,12 @@ function populatePortfolio () {
 
 
     _accordionId = 'Portfolio'+ (i+1);
-    _accordionStr += '<div class="accordionItem accordion portfolio-accordion portfolio-toggle  show" id="'+_accordionId+'">';
+    _accordionStr += '<div class="accordionItem accordion portfolio-accordion portfolio-toggle show row" id="'+_accordionId+'">';
 
     var _portfolioItems = _portfolioArr[i].portfolioItem;
     for( var j=0; j<_portfolioItems.length; j++) {
-      _sidebarStr += '<a href="#card'+_accordionId+'_'+(j+1)+'"><div class="portfolio-trigger accordion-trigger list-group-item" data-target="'+ (i+1) +'" id="portfoliosideItem_'+(i+1)+'">'+_portfolioItems[j].title+'</div></a>';
-      _accordionStr += '<div data-aos="fade-up" data-aos-duration="500" class="card active anchor" id="card'+_accordionId+'_'+(j+1)+'"> <button aria-controls="'+_accordionId+'_'+(j+1)+'" aria-expanded="false" class="btn btn-link" data-target="#'+_accordionId+'_'+(j+1)+'" data-toggle="" type="button"> <div class="card-header" id="'+_accordionId+'-'+(j+1)+'"> <div class="row justify-content-between align-items-center"> <div class="col"> <h2>'+_portfolioItems[j].title+'</h2> <img src="assets/images/header@2x.png"> </div> </div> </div> </button> <div aria-labelledby="'+_accordionId+'-'+(j+1)+'" class=" show " id="'+_accordionId+'_'+(j+1)+'"> <a href="portfolio/'+_portfolioItems[j].link+'.html" target="_blank"> <div class="card-body bg-black"> <img src="assets/images/portfolio/thumbnails/Thumbnail_'+_portfolioItems[j].image+'"> <div class="tags">'+_portfolioItems[j].tags+'</div> </div> </a> </div> </div>';
+      _sidebarStr += '<a href="#card'+_accordionId+'_'+(j+1)+'"><div class="portfolio-trigger accordion-trigger list-group-item" data-target="'+ (i+1) +'" id="portfoliosideItem_'+(i+1)+'">'+"→ "+_portfolioItems[j].title+'</div></a>';
+      _accordionStr += '<div data-aos="fade-up" data-aos-duration="500" class="card active anchor col-12" id="card'+_accordionId+'_'+(j+1)+'"> <button aria-controls="'+_accordionId+'_'+(j+1)+'" aria-expanded="false" class="btn btn-link" data-target="#'+_accordionId+'_'+(j+1)+'" data-toggle="" type="button"> <div class="card-header" id="'+_accordionId+'-'+(j+1)+'"> <div class="row justify-content-between align-items-center"> <div class="col"> <h2>'+_portfolioItems[j].title+'</h2> <img src="assets/images/header@2x.png"> </div> </div> </div> </button> <div aria-labelledby="'+_accordionId+'-'+(j+1)+'" class=" show " id="'+_accordionId+'_'+(j+1)+'"> <a href="portfolio/'+_portfolioItems[j].link+'.html" target="_blank"> <div class="card-body bg-black"> <img src="assets/images/portfolio/thumbnails/Thumbnail_'+_portfolioItems[j].image+'"> <div class="tags">'+_portfolioItems[j].tags+'</div> </div> </a> </div> </div>';
     }
       _accordionStr +=  '</div>';
   }
